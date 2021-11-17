29708d1ba1abef95c552a4442aaecec5680bae4b
PhoenixBoisnier <36176999+PhoenixBoisnier@users.noreply.github.com>
2021-11-17 17:58:30 -0500

    New print
    V2

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello Server!");
+        System.out.println("Hello Server, you sexy beast!");
         }
     }
   
